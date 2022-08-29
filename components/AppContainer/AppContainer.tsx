73b5547d3f2db11555edf9c0930aff453648132f
Isaac <isaac.novaes@hotmail.com>
2022-08-29 15:44:55 +0200

    Set up AppContainer

--- components/AppContainer/AppContainer.tsx
+++ components/AppContainer/AppContainer.tsx
@@ -1,32 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { useAppSelector, useAppDispatch } from '../../store/hooks';
-import { increment, incrementBy } from '../../store/counter/counter-slice';
+import { useAppSelector } from '../../store/hooks';
+import StartGame from '../../screens/StartGame';
+import { StyleSheet, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import GameScreen from '../../screens/GameScreen';
 
 const AppContainer = () => {
-    const counter = useAppSelector((state) => state.counter.counter);
-    const dispatch = useAppDispatch();
+    const userNumber = useAppSelector((state) => state.userNumber.userNumber);
 
     return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <Text>{counter}</Text>
-            <StatusBar style='auto' />
-            <Button title='increment' onPress={() => dispatch(increment())} />
-            <Button
-                title='increment by 10'
-                onPress={() => dispatch(incrementBy(10))}
-            />
-        </View>
+        <LinearGradient style={styles.app} colors={['#4e0329', '#ddb52f']}>
+            <ImageBackground
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                source={require('../../assets/images/app-background.png')}
+                resizeMode='cover'
+                style={styles.app}
+                imageStyle={styles.imageBackground}
+            >
+                {userNumber === null ? <StartGame /> : <GameScreen />}
+            </ImageBackground>
+        </LinearGradient>
     );
 };
-export default AppContainer;
 
 const styles = StyleSheet.create({
-    container: {
+    app: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+    },
+    imageBackground: {
+        opacity: 0.18,
     },
 });
+
+export default AppContainer;
