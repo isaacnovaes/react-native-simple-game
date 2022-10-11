93f2db9b42a8a32ff1f25b3fa56e5010f9d09757
Isaac <isaac.novaes@hotmail.com>
2022-10-11 22:16:16 +0200

    Add global colors

---
+++ constants/colors.ts
@@ -0,0 +1,10 @@
+const Color = {
+    white: '#ffffffcb',
+    yellow: '#ddb52f',
+    bordo500: '#72063c',
+    bordo600: '#640233',
+    bordo700: '#4e0329',
+    bordo800: '#3b021f',
+};
+
+export { Color };
