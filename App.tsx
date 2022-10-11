39aafbde3e2b4d37ded739e712ee6d1615d94adf
Isaac <isaac.novaes@hotmail.com>
2022-10-11 22:15:30 +0200

    Add fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
-import AppContainer from './components/AppContainer/AppContainer';
+import AppContainer from './components/AppContainer';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        OpenSansBold: require('./assets/fonts/OpenSans-Bold.ttf'),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        OpenSansRegular: require('./assets/fonts/OpenSans-Regular.ttf'),
+    });
+
+    console.log(fontsLoaded);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <Provider store={store}>
             <AppContainer />
