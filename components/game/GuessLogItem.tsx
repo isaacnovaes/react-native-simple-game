26338db291fffa9a2fe283a020fdb3318f4b6cb5
Isaac <isaac.novaes@hotmail.com>
2022-10-12 21:32:28 +0200

    Add GuessLogItem

---
+++ components/game/GuessLogItem.tsx
@@ -0,0 +1,35 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { Color } from '../../constants/colors';
+
+const styles = StyleSheet.create({
+    item: {
+        borderColor: Color.bordo800,
+        borderWidth: 1,
+        borderRadius: 40,
+        padding: 12,
+        marginVertical: 10,
+        backgroundColor: Color.yellow,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        elevation: 3,
+        shadowColor: 'black',
+        shadowOffset: { width: 1, height: 1 },
+        shadowRadius: 1,
+        shadowOpacity: 0.7,
+    },
+    text: {
+        fontFamily: 'OpenSansRegular',
+    },
+});
+
+const GuessLogItem = (props: { roundNumber: number; guess: number }) => {
+    return (
+        <View style={styles.item}>
+            <Text style={styles.text}>#{props.roundNumber}</Text>
+            <Text
+                style={styles.text}
+            >{`Opponent's guess: ${props.guess}`}</Text>
+        </View>
+    );
+};
+export default GuessLogItem;
--- store/userNumber/userNumber-slice.ts
+++ store/userNumber/userNumber-slice.ts
@@ -2,23 +2,39 @@ import { createSlice } from '@reduxjs/toolkit';
 // eslint-disable-next-line no-duplicate-imports
 import type { PayloadAction } from '@reduxjs/toolkit';
 
+type NumberOrNull = number | null;
+
 interface UserNumberState {
-    userNumber: number | null;
+    userNumber: NumberOrNull;
+    gameOverData: {
+        number: NumberOrNull;
+        rounds: NumberOrNull;
+    };
 }
 
 const initialState: UserNumberState = {
     userNumber: null,
+    gameOverData: {
+        number: null,
+        rounds: null,
+    },
 };
 
 export const counterSlice = createSlice({
     name: 'userNumber',
     initialState,
     reducers: {
-        getUserNumber(state, action: PayloadAction<number | null>) {
+        getUserNumber(state, action: PayloadAction<NumberOrNull>) {
             state.userNumber = action.payload;
+        },
+        getGameOverData(
+            state,
+            action: PayloadAction<typeof initialState.gameOverData>
+        ) {
+            state.gameOverData = action.payload;
         },
     },
 });
 
-export const { getUserNumber } = counterSlice.actions;
+export const { getUserNumber, getGameOverData } = counterSlice.actions;
 export default counterSlice.reducer;
